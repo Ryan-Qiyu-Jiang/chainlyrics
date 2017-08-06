7fd34a8e11ae5db0420b4bc1555c8e310002d5f6
ryanjiang <ryan.qiyu.jiang@gmail.com>
2017-08-06 15:29:16 -0400

    new express static

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static('public'));
 app.use(bodyParser.json());
+
 
   app.listen(app.get('port'), () => {
     console.log('App started on port', app.get('port'));
